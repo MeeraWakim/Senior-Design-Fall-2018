72c1bc49c995297a0eedc121239e136ea3d936ba
MeeraWakim <meerawakim@utexas.edu>
2018-10-31 04:50:21 -0500

    DEBUGGED CODE! works with 5 sensors simultaneously

--- ultrasonic_sensor/ultrasonic_sensor.ino
+++ ultrasonic_sensor/ultrasonic_sensor.ino
@@ -1,15 +1,17 @@
 int trigPin = 0;
- int echoPin = 0;
+int echoPin = 0;
 
 // defines pins numbers
-const int trig1 = 9;
-const int echo1 = 10;
-const int trig2 = 11;
-const int echo2 = 12;
-/*const int trig3 = ;
-const int echo3 = ;
-const int trig4 = ;
-const int echo4 = ;*/
+const int trig1 = 8;
+const int echo1 = 9;
+const int trig2 = 10;
+const int echo2 = 11;
+const int trig3 = 12;
+const int echo3 = 13;
+const int trig4 = 6;
+const int echo4 = 7;
+const int trig5 = 2;
+const int echo5 = 3;
 
 // defines variables
 long duration;
@@ -18,24 +20,27 @@ boolean sensor1 = false;
 boolean sensor2 = false;
 boolean sensor3 = false;
 boolean sensor4 = false;
-  int myTrigPins[] = {trig1, trig2};
-  int myEchoPins[] = {echo1, echo2};
-  int sensorStates[] = {sensor1, sensor2};
+boolean sensor5 = false;
+  const int myTrigPins[] = {trig1, trig2, trig3, trig4, trig5};
+  const int myEchoPins[] = {echo1, echo2, echo3, echo4, echo5};
+  int sensorStates[] = {sensor1, sensor2, sensor3, sensor4, sensor5};
   int savedDistanceVals[] = {0,0};
 
 void setup() {
-  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  for(int i = 0; i < sizeof(myTrigPins)/sizeof(myTrigPins[0]); i++){
+    pinMode(myTrigPins[i], OUTPUT);
+    pinMode(myEchoPins[i], INPUT);
+  }
   Serial.begin(9600); // Starts the serial communication
 
 }
 
 void loop() {
 
-  for (int i = 1; i <= 2; i++) {
+  for (int i = 0; i < sizeof(myTrigPins)/sizeof(myTrigPins[0]); i++) {
     //setting pins in this iteration of the loop
-    trigPin = myTrigPins[i - 1];
-    echoPin = myEchoPins[i - 1];
+    trigPin = myTrigPins[i];
+    echoPin = myEchoPins[i];
 
     // Clears the trigPin
     digitalWrite(trigPin, LOW);
@@ -59,7 +64,7 @@ void loop() {
     Serial.println(distance);
 
     //saving distance/duration values
-    savedDistanceVals[i - 1] = distance;
+    savedDistanceVals[i] = distance;
   }
 
   for (int i = 1; i <= 2; i++) {
@@ -69,7 +74,7 @@ void loop() {
     }
   }
 
-  //vector shit
+  //vector
 
 
 

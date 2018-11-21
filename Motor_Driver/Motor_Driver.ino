6217885e8dc74861eb78abe24805ed7efd1cc975
George Haugen <georgehaugen23@utexas.edu>
2018-11-20 20:31:01 -0600

    added initilization of ultrasonic sensor code variables to motor driver code

--- Motor_Driver/Motor_Driver.ino
+++ Motor_Driver/Motor_Driver.ino
@@ -1,10 +1,42 @@
-//Motor Connections
+//zero ultrasonic sensor mode
+int trigPin = 0;
+int echoPin = 0;
+
+//Motor Connection pin definitions
 int EnA = 10; //right motor speed control: PWM pin on Arduino denoted by "~"
 int EnB = 5;  //left motor speed control: PWM pin on Arduino denoted by "~"
 int In1 = 9;
 int In2 = 8;
 int In3 = 7;
 int In4 = 6;
+
+//Ultrasonic sensor pin definitions
+const int trig1 = 8;
+const int echo1 = 9;
+const int trig2 = 10;
+const int echo2 = 11;
+const int trig3 = 12;
+const int echo3 = 13;
+const int trig4 = 6;
+const int echo4 = 7;
+const int trig5 = 2;
+const int echo5 = 3;
+
+//ultrasonic sensor variable initilization
+long duration;
+int distance;
+boolean sensor1 = false;
+boolean sensor2 = false;
+boolean sensor3 = false;
+boolean sensor4 = false;
+boolean sensor5 = false;
+  const int myTrigPins[] = {trig1, trig2, trig3, trig4, trig5};
+  const int myEchoPins[] = {echo1, echo2, echo3, echo4, echo5};
+  int sensorStates[] = {sensor1, sensor2, sensor3, sensor4, sensor5};
+  int savedDistanceVals[] = {0,0,0,0,0};
+
+
+
 
 void accelerate() // accelerate from rest to maximum speed
 {
@@ -137,15 +169,24 @@ void frontWall()
   noGo();
   checkSurroundings();     
 }
+
 void setup()
 {
   // All motor control pins are outputs
-  pinMode(EnA, OUTPUT);
-  pinMode(EnB, OUTPUT);
-  pinMode(In1, OUTPUT);
-  pinMode(In2, OUTPUT);
-  pinMode(In3, OUTPUT);
-  pinMode(In4, OUTPUT);
+    pinMode(EnA, OUTPUT);
+    pinMode(EnB, OUTPUT);
+    pinMode(In1, OUTPUT);
+    pinMode(In2, OUTPUT);
+    pinMode(In3, OUTPUT);
+    pinMode(In4, OUTPUT);
+
+  //ultrasonic sensor setup
+    for(int i = 0; i < sizeof(myTrigPins)/sizeof(myTrigPins[0]); i++)
+     {
+      pinMode(myTrigPins[i], OUTPUT);
+      pinMode(myEchoPins[i], INPUT);
+     }
+    Serial.begin(9600); // Starts the serial communication
 }
 
 
